b8817d57b55aa4444ecb850427594497f534cf08
Vo Minh Quan <quanvm198@gmail.com>
2024-01-24 09:23:57 +0700

    Porting PWM, Timer 1 and displaying motor speed

--- Fan_Control_Sysyem(Porting)/Source/main.c
+++ Fan_Control_Sysyem(Porting)/Source/main.c
@@ -4,8 +4,53 @@
 #include "SPI.h"
 #include "MAX7219.h"
 #include "IWDG.h"
+#include "Timer.h"
 
-#define LED_PIN     13
+#define LED_PIN         13
+#define ENCODER_PPR     30
+
+/*Motor variable*/
+int counterVal;
+uint16_t position = 0;
+uint16_t old_position = 0;
+uint16_t encoderPPS = 0;
+uint16_t currentFanRPM = 0;
+uint16_t ms_count = 0;
+uint8_t rx_data = 60;
+
+void TIM1_UP_Handler(){
+	if(TIM1->SR & 0x1){  //Check if interrupt flag is set
+		ms_count += 1;
+		
+		/*Calculate RPM every 200ms*/
+		if(ms_count >= 200){
+			position = counterVal / 4;  //Get current position of encoder
+			if (position >= old_position){
+				encoderPPS = (position - old_position) * 5;  
+			}
+			currentFanRPM = (encoderPPS * 60) / ENCODER_PPR;
+			old_position = position;
+            ms_count = 0;
+		}
+		TIM1->SR &= ~1;  //Reset interrupt flag
+	}
+}
+
+void TIM1_Config(){
+	TIM_EnableTimerClock(TIM1);
+	
+	//Set prescaler and ARR value (Timer 1 generate interrupt every 1ms)
+	TIM1->PSC = 72-1;
+	TIM1->ARR = 1000 - 1;
+	
+	//Enable update interrupt
+	TIM1->DIER |= (1 << 0);
+	
+	NVIC_EnableIRQ(TIM1_UP_IRQn);
+	NVIC_SetPriority(TIM1_UP_IRQn, 2);
+	
+	TIM1->CR1 |= (1 << 0);  //Enable timer 1
+}
 
 void ClockInit(void){
     FLASH->ACR |= (0b010 << 0); //Flash latency
@@ -30,8 +75,18 @@ void ToggleLED(){
     GPIO_Toggle(GPIO_C, LED_PIN);
 }
 
+uint16_t GetCounterValue(){
+	return TIM4->CNT;
+}
+
+void Set_DutyCycle(uint8_t DutyCycle){
+	TIM_SetCCRxReg(TIM3,DutyCycle, TIM_Channel_3);
+}
+
+
 int main(void){
     ClockInit();
+    TIM1_Config();
 
     IWDG_Init(IWDG_DIV_64, 3125);
 
@@ -41,9 +96,22 @@ int main(void){
     MAX7219_Clean();
     MAX7219_Init(15, DIGIT_0_TO_7, DECODE_MODE_DISABLE);
 
+	//Init Timer 3 channel 3 IO for PWM functionality
+	GPIO_Init(GPIO_B, 0, AFIO_OUTPUT);	
+	//PWM Init and run
+	TIM_PWM_Init(TIM3, TIM_Channel_3, 72, 100, 50);
+	TIM_PWM_Start(TIM3, TIM_Channel_3);
+	
+	//Encoder Mode Init and run
+	TIM_Encoder_Init(TIM4, SLAVE_EncoderMode_3);
+	TIM_EncoderStart(TIM4);
+
 
     while (1){
-        MAX7219_PrintInt(2024, 4, DIGIT_POSITION_7);
+        counterVal = GetCounterValue(); //Get current counter value from Timer 4	
+        Set_DutyCycle(rx_data);
+
+        MAX7219_PrintInt(currentFanRPM, 4, DIGIT_POSITION_7);
         ToggleLED();
         delay_ms(100);
 
